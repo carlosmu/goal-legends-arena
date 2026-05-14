ed02fa00a048c1f950bbbd93df39b899cc71cdf0
Carlos Muñoz <carlos.damian.munoz@gmail.com>
2026-05-14 00:23:58 -0300

    update + sit spot

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,7 +5,9 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  Blue_Spot = "Blue_Spot",
   POV = "POV",
+  Red_Spot = "Red_Spot",
   Reds = "Reds",
   futbol_arena_glb = "futbol_arena.glb",
 } 
