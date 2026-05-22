408b4c0ed4945a9953b87bd1ece9221a5ed1a6bd
Carlos Muñoz <carlos.damian.munoz@gmail.com>
2026-05-22 20:09:07 -0300

    Update scene/spawner

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,10 +5,13 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  Admin_Tools = "Admin Tools",
   Blue_Spot = "Blue_Spot",
   POV = "POV",
   POsition_Exapmple = "POsition Exapmple",
   Red_Spot = "Red_Spot",
   Reds = "Reds",
+  Sit_Spot = "Sit Spot",
+  Sit_Spots = "Sit_Spots",
   futbol_arena_glb = "futbol_arena.glb",
 } 
